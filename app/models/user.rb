6f09956b48a7ad6097297b28e68f05d451675c17
chanrioooooo <92098301+chanrioooooo@users.noreply.github.com>
2022-01-10 20:23:44 +0900

    新規登録、ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :date_of_birth,   presence: true
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  LAST_NAME_KANA_REGEX = /\A[ァ-ヴー]+\z/u
+  validates_format_of :last_name_kana, with: LAST_NAME_KANA_REGEX
+
+  FIRST_NAME_KANA_REGEX = /\A[ァ-ヴー]+\z/u
+  validates_format_of :first_name_kana, with: FIRST_NAME_KANA_REGEX
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
