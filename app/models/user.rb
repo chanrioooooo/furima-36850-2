e110c6984ff5eda9148ee1f21d587ef15ea66306
chanrioooooo <92098301+chanrioooooo@users.noreply.github.com>
2022-01-12 12:59:30 +0900

    userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :nickname,        presence: true
-  validates :last_name,       presence: true
+  validates :last_name,       presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name,      presence: true
   validates :last_name_kana,  presence: true
   validates :first_name_kana, presence: true
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid'
 
-  LAST_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  LAST_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]/
   validates_format_of :last_name, with: LAST_NAME_REGEX
 
-  FIRST_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  FIRST_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]/
   validates_format_of :first_name, with: FIRST_NAME_REGEX
 
   LAST_NAME_KANA_REGEX = /\A[ァ-ヴー]+\z/u
@@ -22,7 +22,5 @@ class User < ApplicationRecord
   validates_format_of :first_name_kana, with: FIRST_NAME_KANA_REGEX
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :purchases
+         :recoverable, :rememberable, :validatable  has_many :purchases
 end
