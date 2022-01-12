6e47200b88e1dedd3f2a894356ff85cb74c92abc
chanrioooooo <92098301+chanrioooooo@users.noreply.github.com>
2022-01-12 16:16:22 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :nickname,        presence: true
-  validates :last_name,       presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name,       presence: true
   validates :first_name,      presence: true
   validates :last_name_kana,  presence: true
   validates :first_name_kana, presence: true
