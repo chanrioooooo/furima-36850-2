160cd42aec0a823a1636da973e56c3eadbc56dd4
chanrioooooo <92098301+chanrioooooo@users.noreply.github.com>
2022-01-14 00:56:42 +0900

    ビューファイルの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,21 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+    private
+
+  def item_params
+    params.require(:item).permit(:image, :product_name, :explanation, :category_id, :situation_id, :consignor_id, :shipping_area_id, :days_to_ship_id, :price)
+  end
+
+
+  end
+
 end
