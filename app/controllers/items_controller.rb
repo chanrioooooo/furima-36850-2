fc2fd5092e453bc21dd8bc5facd4107e6b2e4439
chanrioooooo <92098301+chanrioooooo@users.noreply.github.com>
2022-01-14 00:01:59 +0900

    :authenticate_user!の適用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,4 @@
-class ItemsController < ApplicationController
+class ItemsController < ApplicationController  
   def item_params
     params.require(:item).permit(:content, :image).merge(item_id: current_item.id)
   end
