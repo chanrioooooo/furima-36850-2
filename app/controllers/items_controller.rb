ccc91957808d07895ccdd06c03622d11b7597805
chanrioooooo <92098301+chanrioooooo@users.noreply.github.com>
2022-01-13 23:13:31 +0900

    アソシエーション、バリデーションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:content, :image).merge(item_id: current_item.id)
   end
+
+  def new
+    @item = Item.new
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :consignor
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+
+  validates :product_name, :explanation, :category, :situation, :consignor, :shipping_area, :days_to_ship, :price, presence: true
+
+  validates :category_id,      numericality: { other_than: 1 , message: "can't be blank" }
+  validates :situation_id,     numericality: { other_than: 1 , message: "can't be blank" }
+  validates :consignor_id,     numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :days_to_ship_id,  numericality: { other_than: 1 , message: "can't be blank" }
+
   has_one_attached :image
   
   devise :database_authenticatable, :registerable,
