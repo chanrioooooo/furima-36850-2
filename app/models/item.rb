e2aabd92fb81ec82bb011a641ee9fa8040b83da2
chanrioooooo <92098301+chanrioooooo@users.noreply.github.com>
2022-01-15 22:07:05 +0900

    必須項目の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,13 +6,14 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :day
 
-  validates :product_name, :explanation, :category, :situation, :consignor, :area, :day, :price, presence: true
+  validates :image, :product_name, :explanation, :category, :situation, :consignor, :area, :day, presence: true
 
   validates :category_id,      numericality: { other_than: 1 , message: "can't be blank" }
   validates :situation_id,     numericality: { other_than: 1 , message: "can't be blank" }
   validates :consignor_id,     numericality: { other_than: 1 , message: "can't be blank" }
   validates :area_id,          numericality: { other_than: 1 , message: "can't be blank" }
   validates :day_id,           numericality: { other_than: 1 , message: "can't be blank" }
+  validates :price,            presence: true
 
   has_one_attached :image
 
