479f99878b8945b2e596c2a38b1aeaa86df8b0b1
chanrioooooo <92098301+chanrioooooo@users.noreply.github.com>
2022-01-18 21:34:01 +0900

    link_toメソッドを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   get 'items', to: 'items#index'
-  resources :items, only: [:new, :create, :index, :show]
-
-
+  resources :items
 end
