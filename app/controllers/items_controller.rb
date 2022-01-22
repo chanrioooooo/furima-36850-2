4436f1e8977b04fbeff3ff6ecac7f1d700db4d6f
chanrioooooo <92098301+chanrioooooo@users.noreply.github.com>
2022-01-22 21:23:02 +0900

    編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user! ,only:[:new, :create, :edit]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
-  before_action :prevent_url, only: [:edit, :update]
+  before_action :prevent_url, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
