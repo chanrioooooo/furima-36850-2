91e584d9226c8e3938ed3b78697229385f0a0820
chanrioooooo <92098301+chanrioooooo@users.noreply.github.com>
2022-01-23 13:01:35 +0900

    削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! ,only:[:new, :create, :edit]
+  before_action :authenticate_user! ,only:[:new, :create, :edit, :destroy]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
   before_action :prevent_url, only: [:edit, :update, :destroy]
 
