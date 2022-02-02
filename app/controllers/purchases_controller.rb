e37c62dcb5f2eac5fc0d53c272cd73e78aa14302
chanrioooooo <92098301+chanrioooooo@users.noreply.github.com>
2022-02-02 20:06:22 +0900

    購入画面のビューファイル編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,29 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :item_find, only: [:index, :create]
 
   def index
     @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:post_code, :area_id, :municipalities, :address, :building, :telephone_num)
+  end
+
+  def item_find
+    @item = Item.find(params[:item_id])
   end
 
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -3,11 +3,11 @@ class PurchaseAddress
   attr_accessor :post_code, :area_id, :municipalities, :address, :building, :telephone_num, :purchase, :user_id, :item_id
 
   with_options presence: true do
-    validates :post_code,   format: { with: /\A\d{3}-\d{4}$|^\d{3}-\d{2}$|^\d{3}\z/ }
-    validates :area_id,       numericality: { greater_than: 1 }
+    validates :post_code,      format: { with: /\A\d{3}-\d{4}$|^\d{3}-\d{2}$|^\d{3}\z/ }
+    validates :area_id,        numericality: { greater_than: 1 }
     validates :municipalities
     validates :address
-    validates :telephone_num, format: { with: /\A\d{10,11}\z/ }
+    validates :telephone_num,  format: { with: /\A\d{10,11}\z/ }
     validates :user_id
     validates :item_id
   end
@@ -15,6 +15,6 @@ class PurchaseAddress
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(post_code: post_code, area_id: area_id, municipalities: municipalities, address: address,
-                            building: building, telephone_num: telephone_num, purchase: purchase)
+                   building: building, telephone_num: telephone_num, purchase_id: purchase.id)
   end
 end
